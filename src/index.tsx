4e1b7c9e59d8b3bc85e807a4853e534030c3674e
soup-bowl <code@soupbowl.io>
2022-12-18 19:56:58 +0000

    404 page.

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import About from './pages/About';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
+import { Error404 } from './pages/_Error';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -18,7 +19,9 @@ root.render(
 				<Route path="/" element={<Layout />}>
 					<Route index element={<Home />} />
 					<Route path="me" element={<About />} />
+					<Route path="about" element={<Navigate replace to="/me" />} />
 					<Route path="projects" element={<Projects />} />
+					<Route path="*" element={<Error404 />} />
 				</Route>
 			</Routes>
 		</HashRouter>
