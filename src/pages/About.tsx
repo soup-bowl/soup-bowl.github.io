71eb734be3fa393f4e3fbed28010d6c819d95be2
soup-bowl <code@soupbowl.io>
2023-07-31 22:39:11 +0100

    Amends to about.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -34,7 +34,10 @@ const About = () => {
 					My languages of choice are <strong>Python</strong> and <strong>TypeScript</strong>. I have several years of experience with <strong>Git</strong> and Git technology platforms, primarily <strong>Azure DevOps</strong>, <strong>GitHub</strong> and <strong>GitLab</strong>, and some experience with <strong>Subversion</strong> and <strong>JIRA</strong>. My cloud of choice is <strong>Azure</strong>, with minor experience in <strong>Amazon Web Services</strong>. 
 				</p>
 				<p>
-					If you wish to see more about me, please <a href="https://blog.soupbowl.io" style={{ fontWeight: 'bold' }}>check out my blog</a>.
+					If you wish to see more about me, please <a href="https://blog.soupbowl.io" style={{ fontWeight: 'bold' }}>check out my blog</a> or <a href="mailto:code@soupbowl.io" style={{ fontWeight: 'bold' }}>contact me</a>.
+				</p>
+				<p style={{ color: '#787878' }}>
+					I don't use social media any more.
 				</p>
 			</div>
 		</ProfileDisplay>
