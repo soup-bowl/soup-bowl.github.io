62ea6d4b6e30e6ec390cdde58e628b30e0e9e6fb
soup-bowl <code@soupbowl.io>
2023-09-28 08:28:00 +0100

    Added Mastodon and Lemmy.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,6 @@
 import styled from "@emotion/styled";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ProfileDisplay = styled.div({
 	padding: 20,
@@ -28,16 +30,18 @@ const About = () => {
 			</div>
 			<div className="description">
 				<p>
-					Hello! My name is <strong>Casey LP</strong> and I am a developer from Hertfordshire, United Kingdom. I work as an <strong>Azure-based DevOps Engineer</strong> for the <strong>National Physical Laboratory</strong>. Outside NPL, I have a particular fondness for emerging technologies and <strong>containerisation</strong>. I can often be found discussing the benefits of <strong>Docker</strong> and <strong>Kubernetes</strong>, and I often spend my free time designing solutions in <strong>Progressive Web App</strong> format. 
+					Hello! My name is <strong>Casey LP</strong> and I am a developer from Hertfordshire, United Kingdom. I work as an <strong>Azure-based DevOps Engineer</strong> for the <strong>National Physical Laboratory</strong>. Outside NPL, I have a particular fondness for emerging technologies and <strong>containerisation</strong>. I can often be found discussing the benefits of <strong>Docker</strong> and <strong>Kubernetes</strong>, and I often spend my free time designing solutions in <strong>Progressive Web App</strong> format.
 				</p>
 				<p>
-					My languages of choice are <strong>Python</strong> and <strong>TypeScript</strong>. I have several years of experience with <strong>Git</strong> and Git technology platforms, primarily <strong>Azure DevOps</strong>, <strong>GitHub</strong> and <strong>GitLab</strong>, and some experience with <strong>Subversion</strong> and <strong>JIRA</strong>. My cloud of choice is <strong>Azure</strong>, with minor experience in <strong>Amazon Web Services</strong>. 
+					My languages of choice are <strong>Python</strong> and <strong>TypeScript</strong>. I have several years of experience with <strong>Git</strong> and Git technology platforms, primarily <strong>Azure DevOps</strong>, <strong>GitHub</strong> and <strong>GitLab</strong>, and some experience with <strong>Subversion</strong> and <strong>JIRA</strong>. My cloud of choice is <strong>Azure</strong>, with minor experience in <strong>Amazon Web Services</strong>.
 				</p>
 				<p>
-					If you wish to see more about me, please <a href="https://blog.soupbowl.io" style={{ fontWeight: 'bold' }}>check out my blog</a> or <a href="mailto:code@soupbowl.io" style={{ fontWeight: 'bold' }}>contact me</a>.
+					If you wish to see more about me, please <a href="https://blog.soupbowl.io" style={{ fontWeight: 'bold' }}>check out my blog</a>.
 				</p>
-				<p style={{ color: '#787878' }}>
-					I don't use social media any more.
+				<p style={{ fontSize: "1.25em"}}>
+					<a href="mailto:code@soupbowl.io" style={{ color: '#787878' }}>
+						<FontAwesomeIcon icon={faEnvelope} />
+					</a>
 				</p>
 			</div>
 		</ProfileDisplay>
