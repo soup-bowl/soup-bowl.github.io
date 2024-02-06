81ca5e305f647370bc5df87c8d0b52e1d4bd250e
Casey LP <code@soupbowl.io>
2024-02-06 08:22:21 +0000

    Argh this bothered me...

--- portfolio/src/Home.tsx
+++ portfolio/src/Home.tsx
@@ -142,7 +142,7 @@ const Home = () => {
 							<img src="https://files.soupbowl.io/i/logo.webp" />
 						</div>
 						<div>
-							<h1 style={{ fontSize: '48px' }}>soupbowl</h1>
+							<h1 style={{ fontSize: '48px' }}>Soupbowl</h1>
 							<p><strong>DevOps Engineer</strong> from <strong>Hertfordshire, UK</strong></p>
 						</div>
 					</div>
