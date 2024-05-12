cc205a0d5e3ded2c4c9c4db256f99e87a133e27f
soup-bowl <code@soupbowl.io>
2024-05-12 22:05:22 +0100

    Bouncy arrow thing.

--- portfolio/src/segments/Banner.tsx
+++ portfolio/src/segments/Banner.tsx
@@ -1,3 +1,25 @@
+import { keyframes } from "@emotion/react";
+import styled from "@emotion/styled";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const bounce = keyframes`
+	0%, 100% {
+		transform: translateY(0);
+	}
+	50% {
+		transform: translateY(-10px);
+	}
+`;
+
+export const ScrollReminder = styled.div({
+	position: 'absolute',
+	right: 10,
+	bottom: 0,
+	fontSize: '5rem',
+	animation: `${bounce} 3s infinite`,
+});
+
 const Banner = () => {
 	return (
 		<div>
@@ -8,6 +30,9 @@ const Banner = () => {
 				<h1 style={{ fontSize: '48px' }}>Soupbowl</h1>
 				<p><strong>DevOps Engineer</strong> from <strong>Hertfordshire, UK</strong></p>
 			</div>
+			<ScrollReminder>
+				<FontAwesomeIcon icon={faArrowDown} />
+			</ScrollReminder>
 		</div>
 	);
 }
