f3cc2ac116c64f55f046268bd7ada2e185ba90f5
soup-bowl <code@soupbowl.io>
2024-10-25 23:05:35 +0100

    Display the page title.

--- .crossnote/parser.js
+++ .crossnote/parser.js
@@ -3,6 +3,25 @@
 	// https://shd101wyy.github.io/markdown-preview-enhanced/#/extend-parser
 
 	onWillParseMarkdown: async function(markdown) {
+		const frontMatterRegex = /^---\n([\s\S]*?)\n---\n/;
+		const frontMatterMatch = markdown.match(frontMatterRegex);
+
+		// Handle the title from the front matter.
+		let title = '';
+		if (frontMatterMatch) {
+			const frontMatter = frontMatterMatch[1];
+			const titleMatch = frontMatter.match(/title:\s*["']?(.+?)["']?$/m);
+			if (titleMatch) {
+				title = titleMatch[1].trim();
+			}
+
+			markdown = markdown.replace(frontMatterRegex, '');
+		}
+
+		if (title) {
+			markdown = `# ${title}\n\n${markdown}`;
+		}
+
 		// Replace Hugo ref shortcodes with a placeholder URL format
 		markdown = markdown.replace(/{{<\s*ref\s*"([^"]+)"\s*>}}/g, (match, p1) => {
 				return `[Link](${p1})`;
