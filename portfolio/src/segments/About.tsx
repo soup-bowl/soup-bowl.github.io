e84ed7790487ccf3bc516a7165c513193a920989
Casey LP <code@soupbowl.io>
2025-01-04 11:03:50 +0000

    Update About.tsx

--- portfolio/src/segments/About.tsx
+++ portfolio/src/segments/About.tsx
@@ -19,9 +19,9 @@ const About = () => {
 					designing solutions in <strong>Progressive Web App</strong> format.
 				</p>
 				<p>
-					My languages of choice are <strong>Python</strong> and <strong>TypeScript</strong>. I have several
-					years of experience with <strong>Git</strong> and Git technology platforms, primarily{" "}
-					<strong>Azure DevOps</strong>, <strong>GitHub</strong> and <strong>GitLab</strong>, and some
+					My primary languages are <strong>C#</strong> and <strong>TypeScript</strong>, with experience in similar
+					languages too. I have several years of experience with <strong>Git</strong> and Git technology platforms,
+					primarily{" "} <strong>Azure DevOps</strong>, <strong>GitHub</strong> and <strong>GitLab</strong>, and some
 					experience with <strong>Subversion</strong> and <strong>JIRA</strong>. My cloud of choice is{" "}
 					<strong>Azure</strong>, with minor experience in <strong>Amazon Web Services</strong>.
 				</p>
