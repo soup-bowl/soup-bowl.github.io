c86aa92af912310155848215d9f6a65f1a641111
Casey LP <code@soupbowl.io>
2025-12-02 08:56:15 +0000

    Fix formatting in About.tsx description

--- portfolio/src/segments/About.tsx
+++ portfolio/src/segments/About.tsx
@@ -13,7 +13,7 @@ const About = () => {
 			<div className="description">
 				<p>
 					Hello! My name is <strong>Casey LP</strong> and I am a developer from Hertfordshire, United Kingdom.
-					I work as an <strong>Azure-based DevOps Engineer. I have a particular fondness for
+					I work as an <strong>Azure-based DevOps Engineer</strong>. I have a particular fondness for
 					emerging technologies and <strong>containerisation</strong>. I can often be found discussing the
 					benefits of <strong>Docker</strong> and <strong>Kubernetes</strong>, and I often spend my free time
 					designing solutions in <strong>Progressive Web App</strong> format.
