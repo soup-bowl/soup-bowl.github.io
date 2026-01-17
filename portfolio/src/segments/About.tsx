0d7b3556c479a80546b80511d27f82317d326419
Casey LP <11209477+soup-bowl@users.noreply.github.com>
2026-01-17 23:03:21 +0000

    Linted about

--- portfolio/src/segments/About.tsx
+++ portfolio/src/segments/About.tsx
@@ -13,10 +13,10 @@ const About = () => {
 			<div className="description">
 				<p>
 					Hello! My name is <strong>Casey LP</strong> and I am an engineer from Hertfordshire, United Kingdom.
-					I work as an <strong>Azure-based DevOps Engineer</strong>. I have a particular fondness for
-					emerging technologies and <strong>containerisation</strong>. I can often be found discussing the
-					benefits of <strong>containers</strong>, and I often spend my free time
-					building pipelines, deployment code and microservices.
+					I work as an <strong>Azure-based DevOps Engineer</strong>. I have a particular fondness for emerging
+					technologies and <strong>containerisation</strong>. I can often be found discussing the benefits of{" "}
+					<strong>containers</strong>, and I often spend my free time building pipelines, deployment code and
+					microservices.
 				</p>
 				<p>
 					If you wish to see more about me, please{" "}
