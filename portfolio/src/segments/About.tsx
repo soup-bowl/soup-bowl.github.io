daa25f0ddf15668b3f0399def75864894afbc923
Casey LP <11209477+soup-bowl@users.noreply.github.com>
2026-01-17 23:01:59 +0000

    Summarised about.

--- portfolio/src/segments/About.tsx
+++ portfolio/src/segments/About.tsx
@@ -12,19 +12,11 @@ const About = () => {
 			</div>
 			<div className="description">
 				<p>
-					Hello! My name is <strong>Casey LP</strong> and I am a developer from Hertfordshire, United Kingdom.
+					Hello! My name is <strong>Casey LP</strong> and I am an engineer from Hertfordshire, United Kingdom.
 					I work as an <strong>Azure-based DevOps Engineer</strong>. I have a particular fondness for
 					emerging technologies and <strong>containerisation</strong>. I can often be found discussing the
-					benefits of <strong>Docker</strong> and <strong>Kubernetes</strong>, and I often spend my free time
-					designing solutions in <strong>Progressive Web App</strong> format.
-				</p>
-				<p>
-					My primary languages are <strong>C#</strong> and <strong>TypeScript</strong>, with experience in
-					similar languages too. I have several years of experience with <strong>Git</strong> and Git
-					technology platforms, primarily <strong>Azure DevOps</strong>, <strong>GitHub</strong> and{" "}
-					<strong>GitLab</strong>, and some experience with <strong>Subversion</strong> and{" "}
-					<strong>JIRA</strong>. My cloud of choice is <strong>Azure</strong>, with minor experience in{" "}
-					<strong>Amazon Web Services</strong>.
+					benefits of <strong>containers</strong>, and I often spend my free time
+					building pipelines, deployment code and microservices.
 				</p>
 				<p>
 					If you wish to see more about me, please{" "}
@@ -36,9 +28,6 @@ const About = () => {
 				<br />
 				<p style={{ textAlign: "center", fontWeight: "bold" }}>Contact me on</p>
 				<ButtonGroup>
-					<NormalButton onClick={() => (window.location.href = "mailto:code@soupbowl.io")}>
-						<FontAwesomeIcon icon={faEnvelope} /> Email
-					</NormalButton>
 					<NormalButton onClick={() => (window.location.href = "https://matrix.to/#/@soupbowl:mozilla.org")}>
 						<FontAwesomeIcon icon={faComment} /> Matrix
 					</NormalButton>
